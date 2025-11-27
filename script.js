22f4017dee2a188f082df0f137decb6518ceffff
Dragos <edragos33@gmail.com>
2025-11-27 08:56:06 +0100

    declared the main variables

---
+++ script.js
@@ -0,0 +1,7 @@
+// Add a new character character button
+const addCharacter = document.querySelector(".addCharacter");
+
+// Select the desired area
+const towerArea = document.querySelector(".towerBtn");
+const forestArea = document.querySelector(".forestBtn");
+const caveArea = document.querySelector(".caveBtn");
