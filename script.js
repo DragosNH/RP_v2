7393397bb125d14c93bef816d646d04625782444
Dragos <edragos33@gmail.com>
2025-11-27 10:04:07 +0100

    remove function added

--- script.js
+++ script.js
@@ -7,14 +7,31 @@ const towerArea = document.querySelector(".towerBtn");
 const forestArea = document.querySelector(".forestBtn");
 const caveArea = document.querySelector(".caveBtn");
 
+// The number of players
 let currentPlayers = 0;
 let maxPlayers = 6;
 
+// Created elements variables
+
 // Add players to the board
 addCharacter.onclick = function () {
-    if(currentPlayers < maxPlayers){
-        let characterCard = `<h2>Player ${currentPlayers}</h2>`
+    if (currentPlayers < maxPlayers) {
+        let characterCard = `<div class="newPlayer">
+            <input type="text" class="charName">
+            <p class="hp">HP:<span class="hpValue"></span></p>
+            <p class="mp">MP:<span class="mpValue"></span></p>
+            <button class="removePlayer removeBtn">×</button>
+        </div>
+        `
         playableCharacters.insertAdjacentHTML("beforeend", characterCard)
         currentPlayers++;
+
     }
 }
+
+playableCharacters.addEventListener("click", function (e) {
+    if (e.target.classList.contains("removeBtn")) {
+        e.target.closest(".newPlayer").remove();
+        currentPlayers--;
+    }
+});
