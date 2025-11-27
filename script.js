46848db8cdbdf26a54d4440ec72c92436d4f9d7e
Dragos <edragos33@gmail.com>
2025-11-27 09:28:44 +0100

    you can add players now

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 // Add a new character character button
 const addCharacter = document.querySelector(".addCharacter");
+const playableCharacters = document.querySelector(".playableCharacters")
 
 // Select the desired area
 const towerArea = document.querySelector(".towerBtn");
 const forestArea = document.querySelector(".forestBtn");
 const caveArea = document.querySelector(".caveBtn");
+
+let currentPlayers = 0;
+let maxPlayers = 6;
+
+// Add players to the board
+addCharacter.onclick = function () {
+    if(currentPlayers < maxPlayers){
+        let characterCard = `<h2>Player ${currentPlayers}</h2>`
+        playableCharacters.insertAdjacentHTML("beforeend", characterCard)
+        currentPlayers++;
+    }
+}
