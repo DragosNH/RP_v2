24ec5801c69b8de4779aa03d45e9f33406bdf4e1
Dragos <edragos33@gmail.com>
2025-11-27 10:12:01 +0100

    hp and mana values added

--- script.js
+++ script.js
@@ -11,24 +11,26 @@ const caveArea = document.querySelector(".caveBtn");
 let currentPlayers = 0;
 let maxPlayers = 6;
 
-// Created elements variables
+// Created elements declared variables
+let hpVal = 100;
+let manaVal = 100;
 
 // Add players to the board
 addCharacter.onclick = function () {
     if (currentPlayers < maxPlayers) {
         let characterCard = `<div class="newPlayer">
             <input type="text" class="charName">
-            <p class="hp">HP:<span class="hpValue"></span></p>
-            <p class="mp">MP:<span class="mpValue"></span></p>
+            <p class="hp">HP:<span class="hpValue">${hpVal}</span></p>
+            <p class="mp">MP:<span class="mpValue">${manaVal}</span></p>
             <button class="removePlayer removeBtn">×</button>
         </div>
         `
         playableCharacters.insertAdjacentHTML("beforeend", characterCard)
         currentPlayers++;
-
     }
 }
 
+// Remove players from the board
 playableCharacters.addEventListener("click", function (e) {
     if (e.target.classList.contains("removeBtn")) {
         e.target.closest(".newPlayer").remove();
