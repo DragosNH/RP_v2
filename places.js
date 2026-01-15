ac82908afd68d36fc88bbad49ed4760d2fe785aa
Dragoş <edragos33@gmail.com>
2026-01-15 08:41:54 +0100

    created the first container

--- places.js
+++ places.js
@@ -1,9 +1,19 @@
 const castleBtn = document.querySelector(".castleBtn");
 const forestBtn = document.querySelector(".forestBtn");
 const caveBtn = document.querySelector(".caveBtn");
+const areas = document.querySelector(".areas");
+
+let currentPlaces = 0;
+const maxPlaces = 1;
 
 const castleAdventure = () => {
-    console.log("clicked")
+    const castleContainer = document.createElement("div");
+    castleContainer.classList.add("area-container");
+
+
+
+    areas.appendChild(castleContainer);
+    currentPlaces++;
 }
 
 castleBtn.addEventListener("click", castleAdventure);
