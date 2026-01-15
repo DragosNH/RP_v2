3e66f448687154e58d471579ac1e1dbe043693d1
Dragoş <edragos33@gmail.com>
2026-01-15 08:17:25 +0100

    small changes

--- places.js
+++ places.js
@@ -2,3 +2,8 @@ const castleBtn = document.querySelector(".castleBtn");
 const forestBtn = document.querySelector(".forestBtn");
 const caveBtn = document.querySelector(".caveBtn");
 
+const castleAdventure = () => {
+    console.log("clicked")
+}
+
+castleBtn.addEventListener("click", castleAdventure);
