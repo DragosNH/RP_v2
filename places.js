c5055e98f1cfe2093379c820dd16802e4730f112
Dragoş <edragos33@gmail.com>
2026-01-19 16:20:57 +0100

    title added to the scene

--- places.js
+++ places.js
@@ -8,20 +8,28 @@ const maxPlaces = 1;
 
 const castleAdventure = () => {
     if(currentPlaces < maxPlaces){
+        // --------- Container ---------
         const castleContainer = document.createElement("div");
         castleContainer.classList.add("area-container");
-
+        
+        // --------- Remove Button ---------
         let removeBtn = document.createElement("button");
         removeBtn.classList.add("removeBtn");
         removeBtn.innerText = "Close";
-    
+        
         removeBtn.onclick = () => {
             castleContainer.remove();
             currentPlaces--;
         }
 
+        // --------- Title ---------
+        const title = document.createElement("h2");
+        title.innerText = "The Haunted Castle"
+        title.classList.add("scene-title");
+
         areas.appendChild(castleContainer);
         castleContainer.appendChild(removeBtn);
+        castleContainer.appendChild(title);
         currentPlaces++;
     } else {
         castleBtn.classList.add("active");
